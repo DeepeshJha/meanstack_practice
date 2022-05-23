5364ec0e914fac182c0e5ced9664f14078ba02e3
Deepesh Jha <djha@manh.com>
2022-05-23 12:56:11 +0530

    Merge branch 'main' of https://github.com/DeepeshJha/meanstack_practice

--- back-end/models/post.js
+++ back-end/models/post.js
@@ -6,3 +6,43 @@ const postSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('Post', postSchema)
+
+
+
+// const { createPool } = require('mysql');
+// const parser = new (require('node-date-parser').default)();
+
+// const pool = createPool({
+//     connectionLimit: 10,
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'password', 
+//     database: 'todo',
+//     dateStrings: 'date'
+// })
+
+// module.exports = pool;
+
+// let todo = {name: "study ego is the enemy", created_at: new Date(), completed: false }
+// pool.query(`insert into todolist set ?`,todo,(err,res) => {
+//     if(err) throw err;
+//     else console.log("results",res)
+// })
+
+// pool.query(`select * from todolist`,(err,res) => {
+//     if(err) throw err;
+//     else console.log("results",res)
+// })
+
+// const mysql = require('mysql')
+// let mysqlCon = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'password', 
+//     database: 'todo',
+//     dateStrings: 'date'
+// })
+// mysqlCon.connect((err) => {
+//     if(!err) console.log("DB connection successful!")
+//     else console.log("DB connection failed! ", JSON.stringify(err,undefined,2))
+// })
--- back-end/server.js
+++ back-end/server.js
@@ -1,63 +1,96 @@
-// const http = require('http');
-// const app = require("./app")
+// // const http = require('http');
+// // const app = require("./app")
 
-// // const server = http.createServer((req, res) => {
-// //     res.end("First Response")
-// // })
+// // // const server = http.createServer((req, res) => {
+// // //     res.end("First Response")
+// // // })
 
-// const port = process.env.PORT || 3000;
-// app.set("port",port)
+// // const port = process.env.PORT || 3000;
+// // app.set("port",port)
+// // const server = http.createServer(app);
+// // server.listen(port)
+
+//                                             /* ******** Update Code ********* */
+
+// const app = require("./app");
+// const debug = require("debug")("node-angular");
+// const http = require("http");
+
+// const normalizePort = val => {
+//   var port = parseInt(val, 10);
+//   if (isNaN(port)) {
+//     // named pipe
+//     return val;
+//   }
+//   if (port >= 0) {
+//     // port number
+//     return port;
+//   }
+//   return false;
+// };
+
+// const onError = error => {
+//   if (error.syscall !== "listen") {
+//     throw error;
+//   }
+//   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+//   switch (error.code) {
+//     case "EACCES":
+//       console.error(bind + " requires elevated privileges");
+//       process.exit(1);
+//       break;
+//     case "EADDRINUSE":
+//       console.error(bind + " is already in use");
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// };
+
+// const onListening = () => {
+//   const addr = server.address();
+//   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+//   debug("Listening on " + bind);
+// };
+
+// const port = normalizePort(process.env.PORT || "3000");
+// app.set("port", port);
+
 // const server = http.createServer(app);
-// server.listen(port)
+// server.on("error", onError);
+// server.on("listening", onListening);
+// server.listen(port);
 
-                                            /* ******** Update Code ********* */
+const fs = require("fs")
 
-const app = require("./app");
-const debug = require("debug")("node-angular");
-const http = require("http");
+// Should return a list of events
+async function getLogs() {
+    const content = await fs.readFileSync("./logs.txt", 'utf8').split('\r\n');
+    let Content = [];
+    content.forEach(line => {
+        line = line.split('\t');
+        lineObj = {
+            date: new Date(line[0]),
+            code: line[2],
+            userId: line[4]
+        };
+        getData(line).then(data => {line.userName = data})
+        Content.push(line);
+    });
+    console.log("---",Content)
+    return Content;
+}
 
-const normalizePort = val => {
-  var port = parseInt(val, 10);
-  if (isNaN(port)) {
-    // named pipe
-    return val;
+function getData(id) {
+    return new Promise((resolve, reject) => {
+      resolve({
+        "366": "Jack",
+        "132": "Joe",
+        "390": "James",
+      });
+      
+    });
   }
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-  return false;
-};
 
-const onError = error => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  debug("Listening on " + bind);
-};
-
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
-
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
+getLogs();
